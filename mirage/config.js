8b3c9a5ad55c0cad344aa495383a0c99cc2d5df1
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-11-06 11:51:19 +0200

    Added auth callback to mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,8 +1,13 @@
 export default function() {
 
-  this.urlPrefix = 'http://localhost:4000';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  this.namespace = 'api';    // make this `api`, for example, if your API is namespaced
-  this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  this.urlPrefix = 'http://localhost:4000';
+
+  this.get('/auth/github/callback', () => {
+    return {user: { email: 'user@example.com' }, token: 'aaafff'};
+  });
+
+  this.namespace = 'api';
+  this.timing = 400;
 
   this.get('/entries');
   this.post('/entries');
@@ -23,4 +28,5 @@ export default function() {
     return [...tags];
 
   });
+
 }
